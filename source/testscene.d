26ec825c2a50139b64e060641488cef3be16409b
Noé Falzon <1331434+Castux@users.noreply.github.com>
2025-05-03 11:45:49 +0300

    Billboard sorting for grass

--- source/testscene.d
+++ source/testscene.d
@@ -3,6 +3,8 @@ import std.math;
 import std.format;
 import std.conv;
 import std.string;
+import std.random;
+import std.algorithm;
 
 import raylib;
 
@@ -10,17 +12,31 @@ import interfaces;
 import colors;
 import img;
 
+struct BillboardInfo
+{
+	Texture2D img;
+	Vector3 pos;
+	double scale;
+	Color color;
+	double dist;
+}
+
+
 class TestScene : Scene
 {
 	Camera3D camera;
+	Vector3[] grassPos;
 
 	this()
 	{
-		camera.position = Vector3(0.0f, 2.0f, 4.0f);    // Camera position
+		camera.position = Vector3(0.0f, 1.75f, 4.0f);    // Camera position
 		camera.target = Vector3(0.0f, 2.0f, 0.0f);      // Camera looking at point
 		camera.up = Vector3(0.0f, 1.0f, 0.0f);          // Camera up vector (rotation towards target)
 		camera.fovy = 60.0f;                                // Camera field-of-view Y
 		camera.projection = CameraProjection.CAMERA_PERSPECTIVE;             // Camera projection type
+
+		foreach(_; 0 .. 1000)
+			grassPos ~= Vector3(uniform(-64.0, 64.0), 1.0, uniform(-64.0, 64.0));
 
 		DisableCursor();
 	}
@@ -32,13 +48,32 @@ class TestScene : Scene
 
 	void draw(int width, int height)
 	{
+		BillboardInfo[] bills;
+
+		void addBill(string img, Vector3 pos, double scale, Color color = Colors.WHITE)
+		{
+			auto dist = Vector3DistanceSqr(pos, camera.position);
+			bills ~= BillboardInfo(Img(img), pos, scale, color, dist);
+		}
+
+		addBill("gepardi1", Vector3(0,1,0), 0.7);
+		addBill("gepardi1", Vector3(10,1,0), 0.6);
+		addBill("gepardi1", Vector3(0,1,12), 0.8);
+
+		foreach(pos; grassPos)
+			addBill("grass", pos, 2.0);
+
+		bills.sort!((a,b) => a.dist > b.dist);
+
 		ClearBackground(Palette.sky);
 
 		BeginMode3D(camera);
 			DrawPlane(Vector3(0.0f, 0.0f, 0.0f), Vector2(1024, 1024), Palette.ochre);
-			DrawBillboard(camera, Img("gepardi1"), Vector3(0,1,0), 2.0f, Colors.WHITE);
-			DrawBillboard(camera, Img("gepardi1"), Vector3(10,1,0), 2.0f, Colors.WHITE);
-			DrawBillboard(camera, Img("gepardi1"), Vector3(0,1,12), 2.0f, Colors.WHITE);
+
+			foreach(bill; bills)
+				DrawBillboard(camera, bill.img, bill.pos, 2.4f, Palette.yellow);
+
+
 		EndMode3D();
 
 
